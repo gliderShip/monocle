90b22761f98645592135ec0f7debf8e5d436806e
gliderShip <erinhima@gmail.com>
2014-04-15 22:04:37 +0200

    implemented score on images

--- lib/brisk/parsers/preview.rb
+++ lib/brisk/parsers/preview.rb
@@ -2,40 +2,70 @@ require 'nokogiri'
 
 module Brisk
   module Parsers
-    module Preview extend self
+    module Preview
+      extend self
 
-    IMG_CSS = {
-        'good_jpg_css'  => 'body img[src^="http"][src*="jpg"]',
-        'good_jpeg_css' => 'body img[src^="http"][src*="jpeg"]',
-        'bad_jpg_css'   => 'body img[src*="jpg"]',
-        'bad_jpeg_css'  => 'body img[src*="jpeg"]',
-        'bad'           => 'img'
-    }
-
-    IMG_SRC = [
-        'src',
-        'data-cfsrc',
-        'data-lazy-src',
-        'data-original',
-        'data-src',
-    ]
-
-    def parse(base)
-      IMG_CSS.each_value { |css_path|
-        images = base.css(css_path)
-        puts images.inspect
-        if !images.empty?
-          images.each { |image|
-            IMG_SRC.each { |src_name|
-              src = image[src_name]
-              if src && !src.include?('blank') && !src.include?('banner')
-                return src
-              end
-            }
-          }
-        end
+      IMG_CSS = {
+          'good_jpg_css' => 'body img[src^="http"][src*="jpg"]',
+          'good_jpeg_css' => 'body img[src^="http"][src*="jpeg"]',
+          'bad_jpg_css' => 'body img[src*="jpg"]',
+          'bad_jpeg_css' => 'body img[src*="jpeg"]',
+          'bad' => 'img'
       }
-    end
+
+      IMG_SRC = [
+          'src',
+          'data-cfsrc',
+          'data-lazy-src',
+          'data-original',
+          'data-src',
+      ]
+
+      $image_pos = 10
+
+
+      def parse(base)
+
+        IMG_CSS.each_value { |css_path|
+          images = base.css(css_path)
+
+          if !images.empty?
+            srcs = []
+
+            images.each { |image|
+              IMG_SRC.each { |src_name|
+                src = image[src_name]
+                if src && !src.include?('blank') && !src.include?('banner')
+                  srcs.push(src)
+                end
+              }
+            }
+
+            srcs = srcs.inject({}) do |hash, src|
+              hash[src] = score(src)
+              hash
+            end
+
+            src, score = srcs.sort_by { |k, v| v }.last
+            return src
+
+
+          end
+        }
+
+
+      end
+
+      def score(src)
+        score = 0
+        score += $image_pos
+        $image_pos -= 1
+        score -= 10 if src.include?('logo')
+        score -= 20 if src.include?('icon')
+        score -= 30 if src.include?('reklama')
+
+        score
+      end
 
     end
   end
